67ace22b9924f0eab1f8b7814900fa0e299d3137
GuilhermeAssisF <assisguilhermefernandes@gmail.com>
2025-11-24 15:27:13 -0300

    correção alert displayFields

--- forms/14154 - Admissao_IRHO/events/displayFields.js
+++ forms/14154 - Admissao_IRHO/events/displayFields.js
@@ -7,213 +7,131 @@ VALIDA KIT - 97
 GERAR KIT - 89
 */
 function displayFields(form, customHTML) {
-  form.setShowDisabledFields(true);
-  form.setHidePrintLink(true);
-  /* Funcoes anexadas ao formulario no momento da redenrizacao da ficha*/
-  customHTML.append(
-    "<script>function getWKNumState(){ return " +
-      getValue("WKNumState") +
-      "; }</script>"
-  );
-  customHTML.append(
-    "<script>function getTodayDate(){ return " +
-      new java.util.Date().getTime() +
-      "; }</script>"
-  );
-  customHTML.append(
-    "<script>function getFormMode(){ return '" +
-      form.getFormMode() +
-      "'; }</script>"
-  );
-  customHTML.append(
-    "<script>function getUser(){ return '" +
-      getValue("WKUser") +
-      "'; }</script>"
-  );
-  customHTML.append(
-    "<script>function getCompany(){ return " +
-      getValue("WKCompany") +
-      "; }</script>"
-  );
+    form.setShowDisabledFields(true);
+    form.setHidePrintLink(true);
+    
+    /* Funcoes anexadas ao formulario no momento da redenrizacao da ficha */
+    customHTML.append("<script>function getWKNumState(){ return " + getValue("WKNumState") + "; }</script>");
+    customHTML.append("<script>function getTodayDate(){ return " + new java.util.Date().getTime() + "; }</script>");
+    customHTML.append("<script>function getFormMode(){ return '" + form.getFormMode() + "'; }</script>");
+    customHTML.append("<script>function getUser(){ return '" + getValue("WKUser") + "'; }</script>");
+    customHTML.append("<script>function getCompany(){ return " + getValue("WKCompany") + "; }</script>");
 
-  var atividade = parseInt(getValue("WKNumState"));
-  // Obtendo o usuario - data e numero da solicitacao
+    var atividade = parseInt(getValue("WKNumState"));
 
-  if (form.getFormMode() != "VIEW" && (atividade == 0 || atividade == 1)) {
-    var hoje = new Date(),
-      dia = hoje.getDate(),
-      mes = hoje.getMonth() + 1;
+    // Tratamento para INÍCIO ou INÍCIO (SALVO)
+    if (form.getFormMode() != "VIEW" && (atividade == 0 || atividade == 1)) {
+        var hoje = new Date();
+        var dia = hoje.getDate();
+        var mes = hoje.getMonth() + 1;
+        var ano = hoje.getFullYear();
 
-    if (dia < 10) {
-      dia = "0" + dia;
+        if (dia < 10) dia = "0" + dia;
+        if (mes < 10) mes = "0" + mes;
+
+        form.setValue("cpDataAbertura", dia + "/" + mes + "/" + ano);
+
+        try {
+            var filterColaborador = new java.util.HashMap();
+            filterColaborador.put("colleaguePK.colleagueId", getValue("WKUser"));
+            var dadosColaborador = getDatasetValues("colleague", filterColaborador);
+
+            if (dadosColaborador != null && dadosColaborador.size() > 0) {
+                var loginColaborador = new Array(dadosColaborador.get(0).get("login"));
+                var DadosSolicitante = DatasetFactory.getDataset("DS_FLUIG_0006", loginColaborador, null, null);
+
+                if (DadosSolicitante != null && DadosSolicitante.rowsCount > 0) {
+                    // DADOS DO SOLICITANTE
+                    form.setValue("cpLoginFluig", dadosColaborador.get(0).get("login"));
+                    form.setValue("cpNomeSolicitante", DadosSolicitante.getValue(0, "NOME"));
+                    form.setValue("cpMatriculaSolicitante", DadosSolicitante.getValue(0, "CHAPA"));
+                    form.setValue("cpFuncaoSolicitante", DadosSolicitante.getValue(0, "CARGO"));
+                    form.setValue("cpEmpresaSolicitante", DadosSolicitante.getValue(0, "NOMEFANTASIA"));
+                    form.setValue("cpDepartamentoObraSolicitante", DadosSolicitante.getValue(0, "SECAO"));
+                    form.setValue("cpSolicitanteColigada", DadosSolicitante.getValue(0, "CODCOLIGADA"));
+                    form.setValue("cpEstadoSolicitante", DadosSolicitante.getValue(0, "UF_SECAO"));
+                }
+            }
+        } catch (e) {
+            log.error("Erro ao buscar dados do solicitante: " + e);
+        }
+
+        form.setValue("TxtCodSITRais", "1");
+        form.setValue("TxtSITRais", "Ativ. normal c/ remun., lic. remun. c/ dir. integr.");
+        form.setValue("TxtCodVINCRais", "1");
+        form.setValue("TxtVINCRais", "Contr. trab., expr. OU tácito p/ prazo indet");
     }
 
-    if (mes < 10) {
-      mes = "0" + mes;
+    // Função auxiliar para buscar nome do usuário com segurança
+    function getColleagueName(userId) {
+        var filter = new java.util.HashMap();
+        filter.put("colleaguePK.colleagueId", userId);
+        var colaborador = getDatasetValues("colleague", filter);
+        if (colaborador != null && colaborador.size() > 0) {
+            return colaborador.get(0).get("colleagueName");
+        }
+        return userId;
     }
 
-    form.setValue("cpDataAbertura", dia + "/" + mes + "/" + hoje.getFullYear());
+    // Preenchimento de responsáveis por atividade
+    if (atividade == 7) {
+        form.setValue("cpRespGestor", getColleagueName(getValue("WKUser")));
+    }
+    if (atividade == 8) {
+        form.setValue("cpRespDiretor", getColleagueName(getValue("WKUser")));
+    }
+    if (atividade == 74) {
+        form.setValue("cpRespRH", getColleagueName(getValue("WKUser")));
+    }
+    if (atividade == 89) {
+        form.setValue("cpAnalistaBPO", getColleagueName(getValue("WKUser")));
+    }
+    if (atividade == 97) {
+        form.setValue("cpRespKIT", getColleagueName(getValue("WKUser")));
+    }
 
-    var filterColaborador = new java.util.HashMap(),
-      dadosColaborador,
-      loginColaborador,
-      DadosSolicitante;
+    // --- TRATATIVA PARA PROBLEMAS DAS DATAS (CORRIGIDO PARA EVITAR ERRO NULLPOINTER) ---
+    var formatoInput = new java.text.SimpleDateFormat("yyyy-MM-dd");
+    var formatoOutput = new java.text.SimpleDateFormat("dd/MM/yyyy");
 
-    filterColaborador.put("colleaguePK.colleagueId", getValue("WKUser"));
-    dadosColaborador = getDatasetValues("colleague", filterColaborador);
-    loginColaborador = new Array(dadosColaborador.get(0).get("login"));
+    // Função helper para converter data apenas se o valor existir e contiver "-"
+    function converterDataIsoParaBr(campo) {
+        var valor = form.getValue(campo);
+        if (valor != null && valor.indexOf("-") > 0) {
+            try {
+                var dataObj = formatoInput.parse(valor);
+                var dataFormatada = formatoOutput.format(dataObj);
+                form.setValue(campo, dataFormatada);
+            } catch (e) {
+                log.error("Erro ao converter data do campo " + campo + ": " + e);
+            }
+        }
+    }
 
-    DadosSolicitante = DatasetFactory.getDataset(
-      "DS_FLUIG_0006",
-      loginColaborador,
-      null,
-      null
-    );
+    // Lista de campos de data para converter
+    var camposData = [
+        "txtAdmissao",
+        "dtDataNascColaborador",
+        "DTEMISSAOIDENT",
+        "DTTITELEITOR",
+        "dtDataEmissaoCartTrab",
+        "DTVENCHABILIT",
+        "DTEMISSAOCNH",
+        "DtEmRIC",
+        "DTRNE",
+        "DtChegBras",
+        "DTEmPrimCNH",
+        "TxtDtContSind"
+    ];
 
-    //DADOS DO SOLICITANTE
-    form.setValue("cpLoginFluig", dadosColaborador.get(0).get("login"));
-    form.setValue("cpNomeSolicitante", DadosSolicitante.getValue(0, "NOME"));
-    form.setValue(
-      "cpMatriculaSolicitante",
-      DadosSolicitante.getValue(0, "CHAPA")
-    );
-    form.setValue("cpFuncaoSolicitante", DadosSolicitante.getValue(0, "CARGO"));
-    form.setValue(
-      "cpEmpresaSolicitante",
-      DadosSolicitante.getValue(0, "NOMEFANTASIA")
-    );
-    form.setValue(
-      "cpDepartamentoObraSolicitante",
-      DadosSolicitante.getValue(0, "SECAO")
-    );
-    form.setValue(
-      "cpSolicitanteColigada",
-      DadosSolicitante.getValue(0, "CODCOLIGADA")
-    );
-    form.setValue(
-      "cpEstadoSolicitante",
-      DadosSolicitante.getValue(0, "UF_SECAO")
-    );
-    //var banco = "Caixa Econômica Federal";
-    //var CodBanco = "104";
-
-    form.setValue("TxtCodSITRais", "1");
-    form.setValue(
-      "TxtSITRais",
-      "Ativ. normal c/ remun., lic. remun. c/ dir. integr."
-    );
-    form.setValue("TxtCodVINCRais", "1");
-    form.setValue(
-      "TxtVINCRais",
-      "Contr. trab., expr. OU tácito p/ prazo indet"
-    );
-  }
-  if (atividade == 7) {
-    filter = new java.util.HashMap();
-    filter.put("colleaguePK.colleagueId", getValue("WKUser"));
-    var colaborador = getDatasetValues("colleague", filter);
-    form.setValue("cpRespGestor", colaborador.get(0).get("colleagueName"));
-  }
-  if (atividade == 8) {
-    filter = new java.util.HashMap();
-    filter.put("colleaguePK.colleagueId", getValue("WKUser"));
-    var colaborador = getDatasetValues("colleague", filter);
-    form.setValue("cpRespDiretor", colaborador.get(0).get("colleagueName"));
-  }
-  if (atividade == 74) {
-    filter = new java.util.HashMap();
-    filter.put("colleaguePK.colleagueId", getValue("WKUser"));
-    var colaborador = getDatasetValues("colleague", filter);
-    form.setValue("cpRespRH", colaborador.get(0).get("colleagueName"));
-  }
-  if (atividade == 89) {
-    filter = new java.util.HashMap();
-    filter.put("colleaguePK.colleagueId", getValue("WKUser"));
-    var colaborador = getDatasetValues("colleague", filter);
-    form.setValue("cpAnalistaBPO", colaborador.get(0).get("colleagueName"));
-  }
-  if (atividade == 97) {
-    filter = new java.util.HashMap();
-    filter.put("colleaguePK.colleagueId", getValue("WKUser"));
-    var colaborador = getDatasetValues("colleague", filter);
-    form.setValue("cpRespKIT", colaborador.get(0).get("colleagueName"));
-  }
-
-  //tratativa para problemas das datas
-  var formatoInput = new java.text.SimpleDateFormat("yyyy-MM-dd");
-  var formatoOutput = new java.text.SimpleDateFormat("dd/MM/yyyy");
-
-  var DatAdm = form.getValue("txtAdmissao");
-  var Nasc = form.getValue("dtDataNascColaborador");
-  var dtEmRG = form.getValue("DTEMISSAOIDENT");
-  var DtTitu = form.getValue("DTTITELEITOR");
-  var EmCarttrabDt = form.getValue("dtDataEmissaoCartTrab");
-  var dtVencCNH = form.getValue("DTVENCHABILIT");
-  var DtCNHEm = form.getValue("DTEMISSAOCNH");
-  var RICDt = form.getValue("DtEmRIC");
-  var RNEDt = form.getValue("DTRNE");
-  var chegada = form.getValue("DtChegBras");
-  var EmCNH = form.getValue("DTEmPrimCNH");
-  var DtContSindical = form.getValue("TxtDtContSind");
-
-  if (DatAdm.search("-") > 0) {
-    var DtmudancaForm = formatoInput.parse(DatAdm);
-    dtAtualFormatado = formatoOutput.format(DtmudancaForm);
-    form.setValue("DatAdm", dtAtualFormatado);
-  }
-  if (Nasc.search("-") > 0) {
-    var DtmudancaForm = formatoInput.parse(Nasc);
-    dtAtualFormatado = formatoOutput.format(DtmudancaForm);
-    form.setValue("Nasc", dtAtualFormatado);
-  }
-  if (dtEmRG.search("-") > 0) {
-    var DtmudancaForm = formatoInput.parse(dtEmRG);
-    dtAtualFormatado = formatoOutput.format(DtmudancaForm);
-    form.setValue("dtEmRG", dtAtualFormatado);
-  }
-  if (DtTitu.search("-") > 0) {
-    var DtmudancaForm = formatoInput.parse(DtTitu);
-    dtAtualFormatado = formatoOutput.format(DtmudancaForm);
-    form.setValue("DtTitu", dtAtualFormatado);
-  }
-  if (EmCarttrabDt.search("-") > 0) {
-    var DtmudancaForm = formatoInput.parse(EmCarttrabDt);
-    dtAtualFormatado = formatoOutput.format(DtmudancaForm);
-    form.setValue("EmCarttrabDt", dtAtualFormatado);
-  }
-  if (dtVencCNH.search("-") > 0) {
-    var DtmudancaForm = formatoInput.parse(dtVencCNH);
-    dtAtualFormatado = formatoOutput.format(DtmudancaForm);
-    form.setValue("dtVencCNH", dtAtualFormatado);
-  }
-  if (DtCNHEm.search("-") > 0) {
-    var DtmudancaForm = formatoInput.parse(DtCNHEm);
-    dtAtualFormatado = formatoOutput.format(DtmudancaForm);
-    form.setValue("DtCNHEm", dtAtualFormatado);
-  }
-  if (RICDt.search("-") > 0) {
-    var DtmudancaForm = formatoInput.parse(RICDt);
-    dtAtualFormatado = formatoOutput.format(DtmudancaForm);
-    form.setValue("RICDt", dtAtualFormatado);
-  }
-  if (RNEDt.search("-") > 0) {
-    var DtmudancaForm = formatoInput.parse(RNEDt);
-    dtAtualFormatado = formatoOutput.format(DtmudancaForm);
-    form.setValue("RNEDt", dtAtualFormatado);
-  }
-  if (chegada.search("-") > 0) {
-    var DtmudancaForm = formatoInput.parse(chegada);
-    dtAtualFormatado = formatoOutput.format(DtmudancaForm);
-    form.setValue("chegada", dtAtualFormatado);
-  }
-  if (EmCNH.search("-") > 0) {
-    var DtmudancaForm = formatoInput.parse(EmCNH);
-    dtAtualFormatado = formatoOutput.format(DtmudancaForm);
-    form.setValue("EmCNH", dtAtualFormatado);
-  }
-  if (DtContSindical.search("-") > 0) {
-    var DtmudancaForm = formatoInput.parse(DtContSindical);
-    dtAtualFormatado = formatoOutput.format(DtmudancaForm);
-    form.setValue("DtContSindical", dtAtualFormatado);
-  }
+    // Executa a conversão segura para todos os campos
+    for (var i = 0; i < camposData.length; i++) {
+        converterDataIsoParaBr(camposData[i]);
+    }
+    
+    // Observação: Os nomes de variáveis do seu código original (ex: "DatAdm", "Nasc") 
+    // estavam sendo usados como NOME DO CAMPO no setValue (ex: form.setValue("DatAdm", ...)).
+    // Isso criava campos novos não mapeados no HTML. 
+    // A função acima (converterDataIsoParaBr) corrige o valor NO PRÓPRIO CAMPO original, 
+    // que é o comportamento padrão esperado para corrigir datas vindas do Mobile ou de integrações.
 }
